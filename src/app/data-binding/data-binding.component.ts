22b47b72ad3a871c70d53c9b4c9a69acecd76d0e
ltakato <ltakato@hotmail.com>
2018-09-13 12:00:27 -0300

    11 - event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,32 @@ export class DataBindingComponent implements OnInit {
   cursoAngular = true;
   urlImagem = 'https://picsum.photos/200/300\n';
 
+  valorAtual = '';
+  valorSalvo = '';
+  isMouseOver = false;
+
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  handleClick() {
+    alert('clicado');
+  }
+
+  onKeyUp(valor) {
+    this.valorAtual = valor;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
